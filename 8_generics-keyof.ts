15c584d9217608093ec8f7ebb63bc7528d7e5ba1
william vegas <elkareem123@gmail.com>
2022-03-13 17:08:24 -0400

    second example generics with keyof

--- 8_generics-keyof.ts
+++ 8_generics-keyof.ts
@@ -13,3 +13,34 @@ const dogs = [
 
 console.log(pluck(dogs, "age"));
 console.log(pluck(dogs, "name"));
+
+// Example 2
+
+interface BaseEvent {
+  time: number;
+  user: string;
+}
+
+interface EventMap {
+  addToCard: BaseEvent & { quantity: number; productId: string };
+  checkout: BaseEvent;
+}
+
+function sendEvent<Name extends keyof EventMap>(
+  name: Name,
+  data: EventMap[Name]
+): void {
+  console.log([name, data]);
+}
+
+sendEvent("addToCard", {
+  productId: "123",
+  quantity: 2,
+  time: 123,
+  user: "user1"
+});
+
+sendEvent("checkout", {
+  time: 123,
+  user: "user1"
+});
